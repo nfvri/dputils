be62ef9b3bd83560d9b852914780fa1c03015d88
Nikos Anastopoulos <n.anastop@gmail.com>
2016-10-21 01:48:03 +0300

    Refactor all scripts to Python

--- util/system.py
+++ util/system.py
@@ -1,18 +1,15 @@
 import logging
 import subprocess
 
-def run(cmd, do_check=True, do_shell=False, get_out=False):
-    '''
 
+def run(cmd, do_check=True, do_shell=False, get_out=False):
+    """
     Args:
         cmd: command list to run, or command string if do_shell == True
         do_check: throw CalledProcessError when exit code != 0
         do_shell: interpret command as shell command
         get_out: return output
-
-    Returns:
-
-    '''
+    """
     if isinstance(cmd, list):
         cmd_str = ' '.join(cmd)
     elif isinstance(cmd, str):
